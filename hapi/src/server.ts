b75c5a958f47d08ff5f67a5b3e86610305fe3736
Gerardo Lima <gerardo.lima@gmail.com>
2023-02-03 08:30:54 +0000

    feat: auth plugin `foo` and routes

--- hapi/src/server.ts
+++ hapi/src/server.ts
@@ -2,6 +2,7 @@ import Hapi from '@hapi/hapi'
 
 import pluginLogger from './lib/logger'
 import pluginAuthBasic from './lib/auth-basic'
+import pluginAuthFoo from './lib/auth-foo'
 import pluginMongo from './lib/mongo'
 
 import hello from './routes/hello'
@@ -9,6 +10,7 @@ import movies from './routes/movies'
 import health from './routes/health'
 import validated from './routes/validated'
 import authBasic from './routes/routes-basic'
+import authFoo from './routes/routes-foo'
 
 
 /**
@@ -27,6 +29,7 @@ export default async (options?: Hapi.ServerOptions): Promise<Readonly<Hapi.Serve
     server.register(pluginLogger),
     server.register(pluginAuthBasic),
     server.register(pluginMongo),
+    server.register(pluginAuthFoo),
   ])
 
   await Promise.all([
@@ -35,6 +38,7 @@ export default async (options?: Hapi.ServerOptions): Promise<Readonly<Hapi.Serve
     server.register(health, {routes: {prefix: '/api/health'}}),
     server.register(validated, {routes: {prefix: '/api/validated'}}),
     server.register(authBasic, {routes: {prefix: '/api/route-basic'}}),
+    server.register(authFoo, {routes: {prefix: '/api/route-foo'}}),
   ])
 
   await server.initialize()
